4766da5e0e3540e4fc515b8e3f35d0812d7455ff
Abhijit Roy <dalvath3700@gmail.com>
2024-01-25 06:52:31 +0530

    Update pulsar.rb

--- pulsar.rb
+++ pulsar.rb
@@ -5,10 +5,6 @@ class Pulsar < Formula
   sha256 "2646d0f68049747e999a2abbbbec6fb0221f9537c14d5d3148577420165e64f5"
   license "MIT"
 
-  depends_on "llvm"
-  depends_on "clang"
-  depends_on "cmake"
-
   def install
     # Extracted binary 'pulsar-macos-aarch64-v0.7.4-alpha' will be named 
     # Rename it to 'pulsar' and copy it to the bin directory
